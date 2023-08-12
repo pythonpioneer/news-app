1e5623d13b9bef721dfb30d00ccd31443352ba8b
Hritik <pypi.pioneer@gmail.com>
2023-08-12 20:11:45 +0530

    card content is now responsive

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,14 +4,15 @@ import { Grid } from '@mui/material';
 export default function NewsItem() {
     return (
         <>
-            <Grid className='container bg-light'>
-                <Grid className="card">
-                    <img className="card-img-top" src="https://st.depositphotos.com/1026550/2711/i/950/depositphotos_27118113-stock-photo-modern-luxury-car.jpg" style={{width: '200px', height: '200px'}} alt="Card image cap" />
-                    <Grid className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </Grid>
+            <Grid container className="container card bg-light">
+                <Grid item xl={6} md={12} xs={12}>
+                    <img className="card-img-top" src="https://st.depositphotos.com/1026550/2711/i/950/depositphotos_27118113-stock-photo-modern-luxury-car.jpg" alt="Card image cap" />
+                </Grid>
+
+                <Grid item className="card-body" xl={6} md={12} xs={12}>
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
                 </Grid>
             </Grid>
         </>
