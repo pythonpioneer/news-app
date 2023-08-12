c3dc9ea893f96a770f5ca199747fae7a248af002
Hritik <pypi.pioneer@gmail.com>
2023-08-12 23:12:26 +0530

    dark mode enabled everywhere on the site

--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -2,37 +2,44 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import NewsItem from './NewsItem';
 
-export default function () {
+/**
+ * This componet is used to show the news.
+ * @param {string} darkMode - This variable is not used here, only passed to NavItem component.
+ * @param {object} colorMode - This object is not used here, only passed to NavItem component.
+ * @returns {JSX.Element} - A JSX element shows News.
+ */
+export default function (props) {
     return (
         <>
           <Grid container spacing={4}>
             <Grid item lg={4} xs={12} sm={6} md={4}>
-                <NewsItem />
+                <NewsItem darkMode={props.darkMode} colorMode={props.colorMode} />
             </Grid>
             <Grid item lg={4} xs={12} sm={6} md={4}>
-                <NewsItem />
+                <NewsItem darkMode={props.darkMode} colorMode={props.colorMode} />
             </Grid>
             <Grid item lg={4} xs={12} sm={6} md={4}>
-                <NewsItem />
+                <NewsItem darkMode={props.darkMode} colorMode={props.colorMode} />
             </Grid>
             <Grid item lg={4} xs={12} sm={6} md={4}>
-                <NewsItem />
+                <NewsItem darkMode={props.darkMode} colorMode={props.colorMode} />
             </Grid>
             <Grid item lg={4} xs={12} sm={6} md={4}>
-                <NewsItem />
+                <NewsItem darkMode={props.darkMode} colorMode={props.colorMode} />
             </Grid>
             <Grid item lg={4} xs={12} sm={6} md={4}>
-                <NewsItem />
+                <NewsItem darkMode={props.darkMode} colorMode={props.colorMode} />
             </Grid>
             <Grid item lg={4} xs={12} sm={6} md={4}>
-                <NewsItem />
+                <NewsItem darkMode={props.darkMode} colorMode={props.colorMode} />
             </Grid>
             <Grid item lg={4} xs={12} sm={6} md={4}>
-                <NewsItem />
+                <NewsItem darkMode={props.darkMode} colorMode={props.colorMode} />
             </Grid>
             <Grid item lg={4} xs={12} sm={6} md={4}>
-                <NewsItem />
+                <NewsItem darkMode={props.darkMode} colorMode={props.colorMode} />
             </Grid>
+            
             
           </Grid>  
         </>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 
-export default function NewsItem() {
+/**
+ * This componet contain the structure of the news.
+ * @param {string} darkMode - This variable is used to set color for day and night mode.
+ * @param {object} colorMode - This object contain all the style color format for changing the color mode.
+ * @returns {JSX.Element} - A JSX element shows news.
+ */
+export default function NewsItem(props) {
     return (
         <>
-            <Grid container md={12} lg={12} xs={12} sm={12} className="container card bg-light">
+            <Grid container className="container card" style={props.darkMode === 'dark' ? props.colorMode.darkModeBody : props.colorMode.lightMode}>
                 <Grid item>
                     <img className="card-img-top" src="https://st.depositphotos.com/1026550/2711/i/950/depositphotos_27118113-stock-photo-modern-luxury-car.jpg" alt="Card image cap" />
                 </Grid>
