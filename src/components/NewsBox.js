4a7c26c4c9aa0182046ad4efa3d2d1c6f0c68e1e
Hritik <pypi.pioneer@gmail.com>
2023-08-12 20:54:37 +0530

    design changes completed, style removed

--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -5,7 +5,7 @@ import NewsItem from './NewsItem';
 export default function () {
     return (
         <>
-          <Grid container spacing={4} style={{marginTop: ''}}>
+          <Grid container spacing={4}>
             <Grid item lg={4} xs={12} sm={6} md={4}>
                 <NewsItem />
             </Grid>
