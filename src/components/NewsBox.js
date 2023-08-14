af8a95b29d6629f4095978ef84472d07116523d3
Hritik <pypi.pioneer@gmail.com>
2023-08-14 13:58:55 +0530

    pagenation, re-rendering issue raised

--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -2,6 +2,7 @@ import { Grid } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import NewsItem from './NewsItem';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 /**
  * This componet is used to show the news.
@@ -17,29 +18,58 @@ export default function (props) {
     const countries = 'in,gb,jp,tw,us';
     const languages = 'hi,en';
 
-    // state variable to show articles
-    const [articles, setArticles] = useState([]);
+    // state variable 
+    const [articles, setArticles] = useState([]);  // to show articles
+    const [page, setPage] = useState('');  // to set the page
+    const [parsedData, setParsedData] = useState([]);  // to store the data from API
+    const [nextPage, setNextPage] = useState('');
 
     // fetching API using fetch then axios
     const updateApiData = async () => {
 
         /* different api keys are here */
-        // const url = `https://newsdata.io/api/1/news?apikey=pub_274779bfb5acff94dbe83253b43a956b05146&country=${countries}&language=${languages}&category=${props.category}&q=${props.searchText}`;
-        const url = `https://newsdata.io/api/1/news?apikey=pub_2760854888b87d2e70e610a41bf0490e639ad&country=${countries}&language=${languages}&category=${props.category}&q=${props.searchText}`;
+        const url = `https://newsdata.io/api/1/news?apikey=pub_274779bfb5acff94dbe83253b43a956b05146&country=${countries}&language=${languages}&category=${props.category}&q=${props.searchText}&page=${page}`;
+        // const url = `https://newsdata.io/api/1/news?apikey=pub_2760854888b87d2e70e610a41bf0490e639ad&country=${countries}&language=${languages}&category=${props.category}&q=${props.searchText}&page=${page}`;
 
         // fetching the data using axios
         axios.get(url)
 
-        // after the fetching the .then method will render the data in the app
-        .then((response) => {
-            setArticles(response.data.results);
-        })
+            // after the fetching the .then method will render the data in the app
+            .then((response) => {
+                setArticles(response.data.results);
+                setNextPage(response.data.nextPage);
+            })
+
+            // this method will catch the error during api fetching
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    // fetch more data from api
+    const fetchMoreData = async () => {
+        console.log("np" + nextPage);
+        setPage(nextPage);
         
-        // this method will catch the error during api fetching
-        .catch(err => { 
-            console.log(err);
-        });
-    }
+        /* different api keys are here */
+        const url = `https://newsdata.io/api/1/news?apikey=pub_274779bfb5acff94dbe83253b43a956b05146&country=${countries}&language=${languages}&category=${props.category}&q=${props.searchText}&page=${page}`;
+        // const url = `https://newsdata.io/api/1/news?apikey=pub_2760854888b87d2e70e610a41bf0490e639ad&country=${countries}&language=${languages}&category=${props.category}&q=${props.searchText}&page=${page}`;
+
+        // fetching the data using axios
+        axios.get(url)
+
+            // after the fetching the .then method will render the data in the app
+            .then((response) => {
+                console.log(response.data);
+                console.log(articles.concat(response.data.results));
+                setArticles(articles.concat(response.data.results));
+            })
+
+            // this method will catch the error during api fetching
+            .catch(err => {
+                console.log(err);
+            });
+    };  
 
     // fetching API after rendering 
     useEffect(() => {
@@ -50,6 +80,13 @@ export default function (props) {
     return (
         <>
             <Grid container spacing={4}>
+                {/* implementing pagenation */}
+                <InfiniteScroll
+                    dataLength={articles?.length}
+                    next={fetchMoreData}
+                    hasMore={articles?.length < 20}
+                    loader={<h2>yes</h2>}
+                ></InfiniteScroll>
 
                 {/* traversing in all artilcles */}
                 {articles?.map((element) => {
@@ -66,5 +103,5 @@ export default function (props) {
 
             </Grid>
         </>
-    )
-}
+    );
+};
